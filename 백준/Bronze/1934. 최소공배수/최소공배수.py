e095f2aec26641962f0f3197286c449a5bc8cb49
zero_min <130297212+zeromin41@users.noreply.github.com>
2024-06-27 15:02:21 +0900

    [Bronze I] Title: 최소공배수, Time: 124 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -6,3 +6,14 @@ for i in range(t):
     print(math.lcm(a,b))    
 
 #파이썬은 math 모듈에 최소공배수 구하는 lcm함수와 최대공약수 구하는 gcd 함수가 있다!!
+#t = int(input())
+
+'''
+for i in range(t):
+    a,b = map(int,input().split())
+    r = a*b
+    while b>0:
+      a,b = b,a%b
+
+    print(r//a)
+'''
