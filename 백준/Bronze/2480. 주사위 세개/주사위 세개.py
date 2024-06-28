449395ff02bf6e826b9fcd78efe968e8c6526bce
zero_min <130297212+zeromin41@users.noreply.github.com>
2024-06-28 14:36:33 +0900

    [Bronze IV] Title: 주사위 세개, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,10 @@
+x=list(map(int,input().split()))
+len(x)==3
+if x[0]==x[1]==x[2]:
+  print(10000+x[0]*1000)
+elif x[0]==x[1] or x[1]==x[2] or x[0]==x[2]:
+  x.sort()
+  print(1000+x[1]*100)
+else:
+  x.sort()
+  print(x[2]*100)
