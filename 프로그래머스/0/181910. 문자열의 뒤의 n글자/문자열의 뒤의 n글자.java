eace4e1c27009c6b918b92b76c32cca90fd94bff
zero_min <130297212+zeromin41@users.noreply.github.com>
2025-02-13 17:15:13 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        
+        return my_string.substring(my_string.length() - n);
+    }
+}
+
+//substring(i, n) : i 부터 n번째 전까지의 문자열을 저장
