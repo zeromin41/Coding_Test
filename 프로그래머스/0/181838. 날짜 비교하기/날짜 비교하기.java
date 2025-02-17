4bec0b529f5521ded1a8fcaf8961c0e37834f5ee
zero_min <130297212+zeromin41@users.noreply.github.com>
2025-02-17 14:34:28 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+         for (int i = 0; i < 3; i++) {
+            if (date1[i] < date2[i]) return 1;
+            if (date1[i] > date2[i]) return 0;
+        }
+        return 0; 
+    }
+}
